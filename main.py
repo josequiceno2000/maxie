69e94e82d8ae2ce50d9f3bbfa18629bbc2f4310c
Jose <divina.familia.nostri@gmail.com>
2025-12-26 20:33:39 -0600

    feat(main): get maxie on screen

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-def main():
-    print("Hello, World!")
+import pgzrun
+import pygame
 
-if __name__ == "__main__":
-    main()
+WIDTH = 800
+HEIGHT = 600
+TITLE = "Maxie"
+
+maxie = Actor('maxie', anchor=('left', 'top'))
+maxie.pos = (100, 100)
+maxie._surf = pygame.transform.scale(maxie._surf, (200, 200))
+
+def draw():
+    screen.clear()  # Purple background
+    maxie.draw()
+    
+pgzrun.go()
