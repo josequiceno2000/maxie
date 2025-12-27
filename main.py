5f9ae1205ca04b56ab794b1f7e327a3ab840a366
Jose <divina.familia.nostri@gmail.com>
2025-12-26 20:44:53 -0600

    feat(main): add title

--- main.py
+++ main.py
@@ -12,5 +12,14 @@ maxie._surf = pygame.transform.scale(maxie._surf, (200, 200))
 def draw():
     screen.clear()  # Purple background
     maxie.draw()
+    screen.draw.text(
+        "Maxie", 
+        (maxie.x + 10, maxie.y - 60), 
+        fontname="margarine_regular",
+        fontsize=46,
+        color="lightblue",
+        shadow=(1, 1),
+        scolor="darkblue",
+        )
     
 pgzrun.go()
